e7a16deb782e59f08f3f0c076c66c1f86c002735
Bartol0220 <barttkon2@gmail.com>
2025-12-15 23:49:55 +0100

    lab6

---
+++ lab6/lab6/src/main/java/agh/ics/linkedlist/model/Guardian.java
@@ -0,0 +1,13 @@
+package agh.ics.linkedlist.model;
+
+public class Guardian {
+    private Element next;
+
+    public Element getNext() {
+        return next;
+    }
+
+    public void setNext(Element next) {
+        this.next = next;
+    }
+}
---
+++ lab6/lab6/src/main/java/agh/ics/linkedlist/model/List.java
@@ -0,0 +1,7 @@
+package agh.ics.linkedlist.model;
+
+public interface List {
+    public boolean contains(Object object);
+
+    public boolean add(Object object);
+}
